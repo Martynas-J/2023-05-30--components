3f2876ccf6835eb4ec0f8579c249102979137c83
Martynas-J <Aurimhas@gmail.com>
2023-05-31 10:00:05 +0300

    v1.1

--- travelContent.js
+++ travelContent.js
@@ -2,18 +2,16 @@ import button from "./button.js";
 import createHtmlElement from "./functions.js";
 
 export default function travelContent(data) {
-    let { imgSrc, title, paragraph, buttonText, buttonLink, mot, amet } = data
+    let { imgSrc, paragraph, buttonText, buttonLink, mot, amet } = data
     const travelContentElement = createHtmlElement("div", "travel-content")
     const travelImgElement = createHtmlElement("div", "travel-img")
     const imgElement = createHtmlElement("div", "ellipse-2")
     const motElement = createHtmlElement("div", "mot")
     const ametElement = createHtmlElement("div", "amet")
 
-    const h1Element = createHtmlElement("h1")
     const pElement = createHtmlElement("p")
     const buttonElement = button(buttonText, buttonLink)
 
-    h1Element.textContent = title
     pElement.textContent = paragraph
     imgElement.src = imgSrc
     imgElement.alt = "ellipse"
@@ -22,6 +20,6 @@ export default function travelContent(data) {
 
     travelImgElement.append(imgElement, motElement, ametElement)
 
-    travelContentElement.append(travelImgElement, h1Element, pElement, buttonElement)
+    travelContentElement.append(travelImgElement, pElement, buttonElement)
     return travelContentElement
 }
